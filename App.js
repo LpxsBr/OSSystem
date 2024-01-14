aeb58d2b678708e3c50ae4b43ef7e19177206b50
LpxsBr <anselmolopes.an@gmail.com>
2024-01-13 23:59:36 -0300

    chore: change folder structure

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import InfoBar from './components/InfoBar';
+import InfoBar from './src/components/InfoBar';
+import Navbar from './src/components/navbar';
 
 export default function App() {
   return (
     <>
-    <InfoBar/>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <InfoBar />
+      <Navbar />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
     </>
   );
 }
